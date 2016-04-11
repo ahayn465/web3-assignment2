f04e6ffe712c5d43e71b033f85658c9288737471
rylan gotto <rgotto2@gmail.com>
2016-04-10 23:09:08 -0600

    adding token interceptor

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('app', ['ui.router']);
+var app = angular.module('app', ['ui.router', 'auth.services']);
+
+app.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.interceptors.push('TokenInterceptor');
+    }]);
+
+
+app.run(function($rootScope, $location, AuthService) {
+    $rootScope.$on("$routeChangeStart", function(event, nextRoute, currentRoute) {
+        if (nextRoute.access.requiredLogin && !AuthService.isLogged) {
+            $location.path("/");
+        }
+    });
+});
