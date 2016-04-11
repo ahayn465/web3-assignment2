3f98669d03367e4fad9303419035cde690bbcd95
rylan gotto <rgotto2@gmail.com>
2016-04-11 02:49:10 -0600

    adding jwt-expiery code, and status responses module

---
+++ server/middleware/jwt-expire.js
@@ -0,0 +1,20 @@
+var jmsg = require('../status-responses');
+/**
+ *
+ * @param req
+ * @param res
+ * @param next
+ *
+ * If JSON Web Token is valid, check the expire time decoded from JWT and test to make sure the token is still valid.
+ * If not set req.auth attribute to undefined.
+ */
+module.exports = function (req, res, next) {
+    if (req.auth) {
+        var date = new Date();
+        var currentTime = date.getTime();
+        if (req.auth.exp < currentTime) {
+            req.auth = undefined;
+        }
+    }
+    next();
+}
---
+++ server/status-responses.js
@@ -0,0 +1,13 @@
+module.exports = {
+    welcome:     {message: 'hooray! welcome to our api!'},
+    login:       {message: 'Logged in'},
+    logout:      {message: 'Logged out'},
+    inv_login:   {message: 'Incorrect user name or password'},
+    user_ex:     {message: 'Username already exists'},
+    email_ex:    {message: 'Email already exists'},
+    create:      {message: 'Post created!'},
+    update:      {message: 'Post updated!'},
+    del:         {message: 'Successfully deleted'},
+    toke_unauth: {message: 'Unauthorized'},
+    sess_exp:    {message: 'Session Expired'}
+}
