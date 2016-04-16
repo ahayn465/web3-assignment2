aad712f5714e5e55803028140c0e2979cae4d042
ahayn465 <ahayn465@mtroyal.ca>
2016-04-15 18:26:36 -0600

    styling and about page

--- client/app/js/main/state.js
+++ client/app/js/main/state.js
@@ -5,37 +5,42 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
       .state('app.login', {
       url: '/',
       templateUrl: 'js/main/templates/login.tpl.html',
-      controller: 'LoginCtrl'
+      controller: 'LoginCtrl', 
+      cache: false
     })
     .state('app.home', {
       url: '/home',
       templateUrl: 'js/main/templates/dash.tpl.html',
       controller: 'DashCtrl',
+      cache: false,
       access: {requiredLogin: true}
     })
     .state('app.book', {
       url: '/books',
       templateUrl: 'js/main/templates/book.tpl.html',
       controller: 'BookCtrl',
+      cache: false,
       access: {requiredLogin: true}
     })
     .state('app.msg', {
       url: '/messages',
       templateUrl: 'js/main/templates/msg.tpl.html',
       controller: 'MsgCtrl',
+      cache: false,
       access: {requiredLogin: true}
     })   
     .state('app.about', {
       url: '/about',
       templateUrl: 'js/main/templates/about.tpl.html',
-      controller: 'AboutCtrl',
-      access: {requiredLogin: false}
+      controller: 'DashCtrl',
+      cache: false
     })    
     .state('app.todo', {
       url: '/todo',
       templateUrl: 'js/main/templates/todo.tpl.html',
       controller: 'ToDoCtrl',
-      access: {requiredLogin: true}
+      access: {requiredLogin: true}, 
+      cache: false
     });
 
     // if none of the above states are matched, use this as the fallback
